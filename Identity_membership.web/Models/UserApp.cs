5a1f742ba21f34e3536a08aa0a24658de8ea52bd
umut can altıntaş <uavnot2001@gmail.com>
2024-01-17 16:05:57 +0300

    Add project files.

---
+++ Identity_membership.web/Models/UserApp.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity_membership.web.Models
+{
+    public class UserApp:IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
