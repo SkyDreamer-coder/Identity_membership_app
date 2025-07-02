78905fd2f6c064c7cef41f1c9552fe17ebac58b6
umut can altıntaş <uavnot2001@gmail.com>
2025-07-02 18:49:25 +0300

    customSettings implementation and options

--- Identity_membership.web/Program.cs
+++ Identity_membership.web/Program.cs
@@ -16,6 +16,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Configuration.AddJsonFile("customSettings.json", optional: true);
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"),opt=>
